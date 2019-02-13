6a58d8d08192ad19c47289b9cb9b6eed64a70c64
Milen Halachev <milenski88@gmail.com>
2019-02-13 15:54:11 +0200

    Initial Commit

---
+++ MHNetworkingKit/MHNetworkingKit.h
@@ -0,0 +1,19 @@
+//
+//  MHNetworkingKit.h
+//  MHNetworkingKit
+//
+//  Created by Milen Halachev on 13.02.19.
+//  Copyright © 2019 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHNetworkingKit.
+FOUNDATION_EXPORT double MHNetworkingKitVersionNumber;
+
+//! Project version string for MHNetworkingKit.
+FOUNDATION_EXPORT const unsigned char MHNetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHNetworkingKit/PublicHeader.h>
+
+
